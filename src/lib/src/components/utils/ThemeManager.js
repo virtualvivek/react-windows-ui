13aaf643736879b531b48264485232d1998b075d
Vivek Verma <virtualiz.me@gmail.com>
2021-02-19 16:49:13 +0530

    updated theme code

--- src/lib/src/components/utils/ThemeManager.js
+++ src/lib/src/components/utils/ThemeManager.js
@@ -7,8 +7,8 @@ var createAlphaAccentColor = function() {
 exports.createAlphaAccentColor = createAlphaAccentColor
 
 var setAppDark = function() {
-    var meta = document.getElementById('statusBarColor')
-    meta.parentNode.removeChild(meta)
+    // var meta = document.getElementById('statusBarColor')
+    // meta.parentNode.removeChild(meta)
     document.head.innerHTML += '<meta name="theme-color" content="#403E41" id="statusBarColor" />'
     document.documentElement.style.setProperty('--color_light_bg','#111111')
     document.documentElement.style.setProperty('--color_dark_text','#FFFFFF')
@@ -28,8 +28,8 @@ var setAppDark = function() {
 exports.setAppDark = setAppDark
 
 var setAppLight = function() {
-    var meta = document.getElementById('statusBarColor')
-    meta.parentNode.removeChild(meta)
+    // var meta = document.getElementById('statusBarColor')
+    // meta.parentNode.removeChild(meta)
     document.head.innerHTML += '<meta name="theme-color" content="#EEE" id="statusBarColor" />'
     document.documentElement.style.setProperty('--color_light_bg','')
     document.documentElement.style.setProperty('--color_dark_text','')
