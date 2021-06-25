55eb893c9cf9b8358f053c046dd3c41b65073dc2
Unluckymichell <39999330+Unluckymichell@users.noreply.github.com>
2021-06-25 15:31:37 +0200

    Update index.d.ts

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -11,35 +11,35 @@ import NavSearchBox from './src/components/NavBar/NavSearchBox'
 import TransitionToLeft from './src/components/NavBar/Transition/TransitionToLeft'
 
 import Accordion from './src/components/Accordion'
-import Alert from './src/components/Alert'
 import AvatarView from './src/components/AvatarView'
+import Alert from './src/components/Alert'
 import Button from './src/components/Button'
-import ButtonFluent from './src/components/ButtonFluent'
 import ButtonIcon from './src/components/ButtonIcon'
+import ButtonFluent from './src/components/ButtonFluent'
 import Card from './src/components/Card'
 import Checkbox from './src/components/Checkbox'
-import ColorPickerItem from './src/components/ColorPickerItem'
 import CommandBar from './src/components/CommandBar'
+import ColorPickerItem from './src/components/ColorPickerItem'
 import Dialog from './src/components/Dialog'
 import Gauge from './src/components/Gauge'
 import HeaderFixedContainer from './src/components/HeaderFixedContainer'
 import ImageView from './src/components/ImageView'
 import InputDate from './src/components/InputDate'
+import InputText from './src/components/InputText'
 import InputSearchBar from './src/components/InputSearchBar'
 import InputSearchBox from './src/components/InputSearchBox'
 import InputSearchSuggestion from './src/components/InputSearchSuggestion'
-import InputText from './src/components/InputText'
 import LinkCompound from './src/components/LinkCompound'
 import LinkCompoundFluent from './src/components/LinkCompoundFluent'
-import ListItem from './src/components/ListItem'
 import LoaderBar from './src/components/LoaderBar'
 import LoaderBusy from './src/components/LoaderBusy'
+import ListItem from './src/components/ListItem'
 import MenuBar from './src/components/MenuBar'
 import ProgressBar from './src/components/ProgressBar'
 import ProgressBarIndeterminate from './src/components/ProgressBarIndeterminate'
 import RadioButton from './src/components/RadioButton'
-import SelectBox from './src/components/SelectBox'
 import SliderBar from './src/components/SliderBar'
+import SelectBox from './src/components/SelectBox'
 import StickyHeader from './src/components/StickyHeader'
 import Switch from './src/components/Switch'
 import SwitchDayNight from './src/components/SwitchDayNight'
@@ -52,41 +52,42 @@ export {
     NavBarLinkFluent,
     NavBarLinkUser,
     NavPageContainer,
-    NavPageContainerInner,
     NavPageContainerRight,
+    NavPageContainerInner,
     NavSearchBox,
     TransitionToLeft,
 
     Accordion,
-    Alert,
     AvatarView,
+    Alert,
     Button,
+    ButtonIcon,
     ButtonFluent,
     ButtonIcon,
     Card,
     Checkbox,
-    ColorPickerItem,
     CommandBar,
+    ColorPickerItem,
     Dialog,
     Gauge,
     HeaderFixedContainer,
     ImageView,
     InputDate,
+    InputText,
     InputSearchBar,
     InputSearchBox,
     InputSearchSuggestion,
-    InputText,
     LinkCompound,
     LinkCompoundFluent,
-    ListItem,
     LoaderBar,
     LoaderBusy,
+    ListItem,
     MenuBar,
     ProgressBar,
     ProgressBarIndeterminate,
     RadioButton,
-    SelectBox,
     SliderBar,
+    SelectBox,
     StickyHeader,
     Switch,
     SwitchDayNight,
