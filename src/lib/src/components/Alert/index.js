88e1eaf1b5f56e47873ba4397fc73d2b17d48cf7
Vivek Verma <virtualiz.me@gmail.com>
2022-10-10 21:13:03 +0530

    Improved UI.

--- src/lib/src/components/Alert/index.js
+++ src/lib/src/components/Alert/index.js
@@ -18,10 +18,12 @@ const Alert = (props) => {
   return (
     <div className={props.isVisible ? "app-alert show" : "app-alert"}
       onClick={(event)=>_onBackdropPress(event)}>
-      <div className="app-alert-modal">
-        <h1>{props.title}</h1>
-        <div className="app-alert-message">{props.message}</div>
-        <div className="app-alert-btn-container">
+      <div className="app-alert-modal" aria-modal="true" role="dialog">
+        <div className="app-alert-header">
+          <h1>{props.title}</h1>
+          <div className="app-alert-message">{props.message}</div>
+        </div>
+        <div className="app-alert-footer">
           {props.children}
         </div>
       </div>
