734ba6adf18cfb16db09a19f0f72078d74fee85b
Vivek Verma <virtualiz.me@gmail.com>
2022-12-27 17:21:56 +0530

    Updates

--- static/js/7.5fd85f94.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-windows-ui-app"]=this["webpackJsonpreact-windows-ui-app"]||[]).push([[7],{383:function(p,i,s){}}]);
-//# sourceMappingURL=7.5fd85f94.chunk.js.map
--- static/js/8.632a1b7f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-windows-ui-app"]=this["webpackJsonpreact-windows-ui-app"]||[]).push([[8],{382:function(p,i,s){}}]);
-//# sourceMappingURL=8.632a1b7f.chunk.js.map
